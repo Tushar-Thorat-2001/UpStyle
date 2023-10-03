b49b06a81201c75aa081d4a303af86ee7503c460
Tushar-Thorat-2001 <58701931+Tushar-Thorat-2001@users.noreply.github.com>
2023-10-04 03:24:22 +0530

    first

---
+++ backend/data/products.js
@@ -0,0 +1,151 @@
+const products = [
+  {
+    name: "Anima Hoodie",
+    image: "/images/01.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Urban Outfitters",
+    category: "Anima Fashion",
+    price: 1499,
+    countInStock: 42,
+    rating: 4.4,
+    numReviews: 2,
+  },
+  {
+    name: "Sasuke Uchiha",
+    image: "/images/02.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Urban Outfitters",
+    category: "Anima Fashion",
+    price: 1999,
+    countInStock: 42,
+    rating: 4.4,
+    numReviews: 2,
+  },
+  {
+    name: "Demon Slayer",
+    image: "/images/03.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Urban Outfitters",
+    category: "Anima Fashion",
+    price: 799,
+    countInStock: 42,
+    rating: 4.4,
+    numReviews: 2,
+  },
+  {
+    name: "Akatsuki t-shirts",
+    image: "/images/04.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Urban Outfitters",
+    category: "Anima Fashion",
+    price: 599,
+    countInStock: 42,
+    rating: 4.4,
+    numReviews: 2,
+  },
+  {
+    name: "Women Floral Print Lounge T-Shirt",
+    image: "/images/female_img_3.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Burberry",
+    category: "Women Fashion",
+    price: 1999,
+    countInStock: 22,
+    rating: 4.6,
+    numReviews: 4,
+  },
+  {
+    name: "Print Crop Lounge Top",
+    image: "/images/female_img_1.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Gucci",
+    category: "Women Fashion",
+    price: 1299,
+    countInStock: 8,
+    rating: 4.8,
+    numReviews: 12,
+  },
+  {
+    name: "Crochet Detail Lightweight Top",
+    image: "/images/female_img_2.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Chanel",
+    category: "Women Fashion",
+    price: 699,
+    countInStock: 6,
+    rating: 4.9,
+    numReviews: 8,
+  },
+  {
+    name: "Printed Polo Collar T-shirt",
+    image: "/images/female_img_4.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Chanel",
+    category: "Women Fashion",
+    price: 999,
+    countInStock: 0,
+    rating: 4.2,
+    numReviews: 5,
+  },
+
+  {
+    name: "Pack of 2 Round Neck T-Shirts",
+    image: "/images/male_img_4.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Urban Outfitters",
+    category: "Men Fashion",
+    price: 799,
+    countInStock: 42,
+    rating: 4.4,
+    numReviews: 2,
+  },
+
+  {
+    name: "Native Youth",
+    image: "/images/male_img_2.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Ralph Lauren",
+    category: "Men Fashion",
+    price: 899,
+    countInStock: 18,
+    rating: 4.5,
+    numReviews: 2,
+  },
+
+  {
+    name: "Juventus Henley Neck Jersey",
+    image: "/images/male_img_1.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Tom Ford",
+    category: "Men Fashion",
+    price: 1499,
+    countInStock: 21,
+    rating: 4.1,
+    numReviews: 3,
+  },
+  {
+    name: "Mavericks Drake Tank Top",
+    image: "/images/male_img_3.jpg",
+    description:
+      "Etiam cursus condimentum vulputate. Nulla nisi orci, vulputate at dolor et, malesuada ultrices nisi. Ut varius ex ut purus porttitor, a facilisis orci condimentum. Nullam in elit et sapien ornare pellentesque at ac lorem.",
+    brand: "Calvin Klein",
+    category: "Men Fashion",
+    price: 1399,
+    countInStock: 25,
+    rating: 3.4,
+    numReviews: 3,
+  },
+];
+
+export default products;
